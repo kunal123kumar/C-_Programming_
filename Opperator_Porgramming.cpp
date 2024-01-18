033f47688ec79c89928a9585e217ff5b4e882e1e
kunal123kumar <154955493+kunal123kumar@users.noreply.github.com>
2024-01-19 00:34:40 +0530

    Add files via upload

---
+++ Opperator_Porgramming.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5,b=4;
+    cout<<"\nThe value of a!=b is:"<<((a!=b)&&(a==b));
+    cout<<"\nThe value of a==b is:"<<(a==b);
+    cout<<"\nThe value of a<=b is:"<<(a<=b);
+    cout<<"\nThe value of a>=b is:"<<(a>=b);
+    cout<<"\nThe value of a<b is:"<<(a<b);
+    cout<<"\nThe value of a>b is:"<<(a>b);
+    return 0;
+}
---
+++ Sum_programing.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    int a,b;
+    cout<<"Enter the value of a :\n";
+    cin>>a;
+    cout<<"Enter the value of b :\n";
+    cin>>b;
+    cout<<"sum of number "<< a+b;
+    return 0;
+}
